bb4bc7764b0804c51f37d471fedd22fc9122be43
Eric Olaru-Hagen <e.olaruhagen@gmail.com>
2026-05-29 20:07:33 -0700

    current adaptive agent implementation. Works alright. Needs tuning on these.
    (1) distance between the agents for board coverage.
    (2) better kill detection in _find_kill()
    (3) patrolling border areas, the set is built and updated just not used in feature extraction
    (4) better ways at committing to offense and lookahead via expectimax.
    (5) weight tuning.

--- pacai/student/capture.py
+++ pacai/student/capture.py
@@ -32,8 +32,14 @@ def create_team() -> list[pacai.core.agentinfo.AgentInfo]:
     Get the agent information that will be used to create a capture team.
     """
 
-    agent1_info = pacai.core.agentinfo.AgentInfo(name = f"{__name__}.InitialReflexOffensive")
-    agent2_info = pacai.core.agentinfo.AgentInfo(name = f"{__name__}.DefensiveAgent")
+    agent1_info = pacai.core.agentinfo.AgentInfo(
+            name = f"{__name__}.UnifiedAgent",
+            my_mode_var = 'agent_1_last_turn_mode',
+            teammate_mode_var = 'agent_2_last_turn_mode')
+    agent2_info = pacai.core.agentinfo.AgentInfo(
+            name = f"{__name__}.UnifiedAgent",
+            my_mode_var = 'agent_2_last_turn_mode',
+            teammate_mode_var = 'agent_1_last_turn_mode')
 
     return [agent1_info, agent2_info]
 
@@ -51,7 +57,7 @@ class InitialReflexOffensive(pacai.agents.greedy.GreedyFeatureAgent):
         self.weights['distance_to_threat'] = 10
         self.weights['distance_to_prey'] = -40.0
         self.weights['nearest_capsule'] = -6.0
-        self.weights['on_home_side'] = -40.0
+        self.weights['off_on_home_side'] = -40.0
         self.weights['on_home_side_unsafe'] = -50.0
         self.weights['have_scared_enemy'] = 1100
         self.weights['oscilating_action'] = -3.0
@@ -70,10 +76,10 @@ def reflex_offensive_features_extractor(
         agent: pacai.core.agent.Agent | None = None,
         **_kwargs: typing.Any) -> pacai.core.features.FeatureDict:
 
-    agent = typing.cast(InitialReflexOffensive, agent)
+    agent = typing.cast(UnifiedAgent, agent)
     state = typing.cast(pacai.capture.gamestate.GameState, state)
 
-    distance_cache = agent.precomputer
+    distance_cache = agent._distances
 
     features = pacai.core.features.FeatureDict()
 
@@ -93,7 +99,7 @@ def reflex_offensive_features_extractor(
     if current_pos is None:
         return features
 
-    features['on_home_side'] = int(state.is_ghost(agent_index = agent.agent_index))
+    features['off_on_home_side'] = int(state.is_ghost(agent_index = agent.agent_index))
     features['on_home_side_unsafe'] = int(state.is_scared(agent_index = agent.agent_index))
 
     threat_positions = []
@@ -209,7 +215,7 @@ class DefensiveAgent(pacai.agents.greedy.GreedyFeatureAgent):
         self.weights['food_distance_sum'] = -2.0
         self.weights['num_invaders_on_same_side'] = -6500.0
         self.weights['invader_distance_to_food'] = 30.0
-        self.weights['on_home_side'] = 1300.0
+        self.weights['def_on_home_side'] = 1300.0
         self.weights['stopped'] = -95.0
 
     def game_start(self, initial_state: pacai.core.gamestate.GameState):
@@ -221,7 +227,7 @@ def extract_defensive_features(
         agent: pacai.core.agent.Agent | None = None,
         **_kwargs: typing.Any) -> pacai.core.features.FeatureDict:
 
-    agent = typing.cast(DefensiveAgent, agent)
+    agent = typing.cast(UnifiedAgent, agent)
     state = typing.cast(pacai.capture.gamestate.GameState, state)
 
     features: pacai.core.features.FeatureDict = pacai.core.features.FeatureDict()
@@ -232,7 +238,7 @@ def extract_defensive_features(
         return features
 
     # Note the side of the board we are on.
-    features['on_home_side'] = int(state.is_ghost(agent_index = agent.agent_index))
+    features['def_on_home_side'] = int(state.is_ghost(agent_index = agent.agent_index))
 
     # Prefer moving over stopping.
     features['stopped'] = int(action == pacai.core.action.STOP)
@@ -279,6 +285,63 @@ def extract_defensive_features(
     if (len(invader_food_distances) > 0):
         features['invader_distance_to_food'] = min(invader_food_distances)
 
+    enemies = list(state.get_opponent_positions(agent_index = agent.agent_index).values())
+
+
+    # - `distance_to_threatened_capsule`
+    #   - A capsule is threatened if an invader is within `CAPSULE_GUARD_RANGE = 6`.
+    #   - If the defender is not scared, prioritize moving toward the threatened capsule instead of directly chasing the invader.
+    #   - If the defender is scared, scared-distance behavior takes precedence.
+    # - `distance_from_attacker_to_powerpellet`
+    #   - only weigh this feature heavely if the offensive agent is close to the power pellet.
+    #   - if the offensive agent is close to the power pellet, and the defensive agent is close to the power pellet, then the defensive agent should move toward the power pellet and guard it if and only if the defender is close to the power pellet.
+    #   - we get the distance of the attacker to its nearist power pellet, and the distance of the defender to that same power pellet.
+    #   - first, if the attacker is not even near its nearist power pellet, we can just not have this feature activate, and we only want to activate this feature if the attacker is within a certain range of its nearest power pellet. If the attacker is close, then we determine the differnence in distnaces between the defender and the attacker relative to the power pellet. if we are close, the reward is positive, and if the attacker is close and we are far, the reward is negative.
+    # protect a power pellet if they are close to it
+    pellet_range = 6
+    power_pellets = []
+    my_side = state._team_modifier(agent.agent_index)
+    for pellet in state.board.get_marker_positions(pacai.pacman.board.MARKER_CAPSULE):
+        if (state._team_side(position = pellet) == my_side):
+            power_pellets.append(pellet)
+
+    closest_pellet = None
+    attacker_dist = None
+    for pellet in power_pellets:
+        dist = get_nearest_distance(enemies, agent._distances, pellet)
+        if (dist is not None and dist <= pellet_range
+                and (attacker_dist is None or dist < attacker_dist)):
+            closest_pellet = pellet
+            attacker_dist = dist
+
+    if (closest_pellet is not None and attacker_dist is not None and not is_scared):
+        my_dist = agent._distances.get_distance(current_position, closest_pellet)
+        if (my_dist is not None):
+            features['distance_from_attacker_to_powerpellet'] = (attacker_dist - my_dist)
+
+            invader_dist = get_nearest_distance(invader_pos_list, agent._distances, closest_pellet)
+            if (invader_dist is not None and invader_dist <= pellet_range):
+                features['distance_to_threatened_capsule'] = my_dist
+
+    # - `oscilating_peanlity`
+    #   - This feature's goal is to give a higher and higher penalty as the agent oscillates back and forth.
+    #   - Have a momentum variable that stores the penalty of the current oscillation of the agent
+    #   - We analyze the previous three positions of the agent. If the first position and the last position are the same, that means we just made an oscillation, which would trigger the momentum variable to be multiplied by 1.2. Otherwise if it's not an oscillation, which would be the scenario if position 1 and position 3 are not the same, then we multiply the momentum variable by 0.9. We don't make the momentum variable go below 0.5
+    # give more of a penalty if it keeps going back and forth
+    loop_penalty = agent.extra_storage.get('loop_penalty', 0.5)
+    num_positions = len(agent.last_positions)
+    if (agent.extra_storage.get('last_loop_check') != num_positions):
+        if (num_positions >= 3 and agent.last_positions[-3] == agent.last_positions[-1]):
+            loop_penalty *= 1.2
+        else:
+            loop_penalty = max(0.5, loop_penalty * 0.9)
+
+        agent.extra_storage['loop_penalty'] = loop_penalty
+        agent.extra_storage['last_loop_check'] = num_positions
+
+    if (num_positions >= 2 and current_position == agent.last_positions[-2]):
+        features['oscilating_peanlity'] = loop_penalty
+
     return features
 
 
@@ -302,11 +365,41 @@ def conditional_feature_extractor(
         agent: pacai.core.agent.Agent | None = None,
         **_kwargs: typing.Any
 ) -> pacai.core.features.FeatureDict:
-    
-    agent = typing.cast(DefensiveAgent, agent)
+
+    agent = typing.cast(UnifiedAgent, agent)
     state = typing.cast(pacai.capture.gamestate.GameState, state)
 
-    return {}
+    current_mode = globals()[agent.my_mode]
+    teammate_mode = agent.read_teamate_mode()
+
+    invaders = state.get_invader_positions(agent_index=agent.agent_index)
+    num_invaders = len(invaders)
+
+    # check if we want to prefer attacking by being in the offense window
+    in_post_kill_window = False
+    if agent.turns_since_kill is not None:
+        if agent.turns_since_kill < POST_KILL_OFFENSIVE_WINDOW:
+            in_post_kill_window = True
+
+    if current_mode is not None and agent.turns_in_mode < MIN_TURNS_PER_MODE:
+        mode = current_mode
+
+    # defend when we have invaders and no existing defender
+    elif num_invaders > 0 and teammate_mode != 'defensive':
+        mode = 'defensive'
+
+    elif in_post_kill_window:
+        mode = 'offensive'
+    elif num_invaders == 0 and teammate_mode != 'offensive':
+        mode = 'offensive'
+    else:
+        mode = 'defensive'  # default we could tune, but push towards offensive
+
+    agent.write_used_mode(mode)
+
+    if mode == 'offensive':
+        return reflex_offensive_features_extractor(state, action, agent=agent)
+    return extract_defensive_features(state, action, agent=agent)
 
 
 class UnifiedAgent(pacai.agents.greedy.GreedyFeatureAgent):
@@ -322,9 +415,6 @@ class UnifiedAgent(pacai.agents.greedy.GreedyFeatureAgent):
                 pacai.search.distance.DistancePreComputer())
         """ Precompute distances. """
 
-        self.my_mode = my_mode_var
-        self.teammate_mode = teammate_mode_var
-
         self.border_crossings: set[pacai.core.board.Position] = set()
         self.team_side = typing.Literal['left', 'right']
 
@@ -334,14 +424,52 @@ class UnifiedAgent(pacai.agents.greedy.GreedyFeatureAgent):
         self.weights['food_distance_sum'] = -2.0
         self.weights['num_invaders_on_same_side'] = -6500.0
         self.weights['invader_distance_to_food'] = 30.0
-        self.weights['on_home_side'] = 1300.0
+        self.weights['def_on_home_side'] = 1300.0
         self.weights['stopped'] = -95.0
+        self.weights['distance_to_threatened_capsule'] = -45.0
+        self.weights['distance_from_attacker_to_powerpellet'] = 45.0
+        self.weights['oscilating_peanlity'] = -15.0 
 
         # offensive mode features go here
+        self.precomputer = pacai.search.distance.DistancePreComputer()
+        self.threat_detection_range = 10
+        self.safe_food_distance = 5
+        self.weights['score'] = 100.0
+        self.weights['distance_to_food'] = -22.0
+        self.weights['distance_to_non_defended_food'] = -3.5
+        self.weights['distance_to_threat'] = 10
+        self.weights['distance_to_prey'] = -40.0
+        self.weights['nearest_capsule'] = -6.0
+        self.weights['off_on_home_side'] = -40.0
+        self.weights['on_home_side_unsafe'] = -50.0
+        self.weights['have_scared_enemy'] = 1100
+        self.weights['oscilating_action'] = -3.0
+        self.weights['food_eaten_by_action'] = 1200.0
+        self.weights['food_cluster_nearby'] = 5.0
+
+
+        # shared features
+        self.weights['distance_to_teamate'] = 10
+        
+
+        # stateful vars for conditional logic on feature extraction
+        self.turns_in_mode: int = 0
+        self.num_kills: int = 0
+        self.turns_since_kill: int | None = None  # init to none when no kills yet
+        self.prev_enemy_positions: dict[int, pacai.core.board.Position] = dict()
+        # represent the last seen mode for current and team agent (from the last turn)
+        self.my_mode = my_mode_var
+        self.teammate_mode = teammate_mode_var
+
 
     def game_start(self, initial_state: pacai.core.gamestate.GameState):
+        self._init_border_crossing_positions(initial_state)
         self._distances.compute(initial_state.board)
+        state = typing.cast(pacai.capture.gamestate.GameState, initial_state)
+        self.prev_enemy_positions = state.get_opponent_positions()
+        super().game_start(initial_state)
     
+
     # helper methods for reading module level shared state variables
     def read_teamate_mode(self) -> typing.Literal['offensive' , 'defensive'] | None:
         return globals()[self.teammate_mode]
@@ -352,7 +480,83 @@ class UnifiedAgent(pacai.agents.greedy.GreedyFeatureAgent):
     def _init_border_crossing_positions(self, initial_state: pacai.core.gamestate.GameState):
         team_side = initial_state._team_modifier(agent_index=self.agent_index)
 
+        width = initial_state.board.width
+        height_bound = initial_state.board.height
+        center_column = None
+
         if team_side == -1:
             self.team_side = 'left'
+            center_column = (width // 2) - 1
         elif team_side == 1:
-            self.team_side = 'rig'
+            self.team_side = 'right'
+            center_column = (width // 2)
+
+        if center_column is None:
+            return
+        # now initialize border crossings iterating through non-wall positions on boundary_column
+        for pos_y in range(height_bound):
+            pos = pacai.core.board.Position(pos_y, center_column)
+            if not initial_state.board.is_wall(pos):
+                self.border_crossings.add(pos)
+        return
+
+    def _find_kill(self, state: pacai.core.gamestate.GameState) -> bool:
+        state = typing.cast(pacai.capture.gamestate.GameState, state)
+        # skip if dead, waste of time.
+        current_pos = state.get_agent_position(self.agent_index)
+        if current_pos is None:
+            return False
+        # otherwise, if my current position is the prev pos of enenmy, we found a kill.
+        # more complex logic (enemy moves into us) to be handled later
+        # current_opponent_positions: dict[int, pacai.core.board.Position] = state.get_opponent_positions()
+        for pos in self.prev_enemy_positions.values():
+            if current_pos == pos:
+                return True
+        return False
+
+    # this one is simply for updating teh stateful vars on the num kills and turns since kill
+    def update_stateful_vars(self, state: pacai.core.gamestate.GameState):
+        state = typing.cast(pacai.capture.gamestate.GameState, state)
+        if self._find_kill(state):
+            self.num_kills += 1
+            self.turns_since_kill = 0
+        elif self.turns_since_kill is not None:
+            # bumpt the turns since kill
+            self.turns_since_kill += 1
+
+    def distance_to_teamate(self, state: pacai.core.gamestate.GameState) -> float | None:
+        capture_state = typing.cast(pacai.capture.gamestate.GameState, state)
+        teamate_pos = list(capture_state.get_ally_positions().values())[0]
+        current_pos = state.get_agent_position(agent_index=self.agent_index)
+        if teamate_pos is None:
+            return None
+        return self._distances.get_distance(current_pos, teamate_pos)
+
+
+
+    def get_action(self, state: pacai.core.gamestate.GameState) -> pacai.core.action.Action:
+        self.update_stateful_vars(state)
+        last_mode = globals()[self.my_mode]
+        action = super().get_action(state)
+        # flow in get_action can change self.my_mode use this for internal turns in mode
+        if last_mode == globals()[self.my_mode]:
+            self.turns_in_mode += 1
+        else:
+            self.turns_in_mode = 0
+
+        self.prev_enemy_positions = state.get_opponent_positions()
+        return action
+    
+
+
+        
+
+
+
+    
+
+        
+    
+        
+
+    
